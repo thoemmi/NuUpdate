b0e0ac47d0e6244918a6054de8a0f14371c83bb0
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-08-26 23:24:16 +0200

    creating AssemblyVersion etc from "git describe" automatically

--- NuUpdate/Properties/AssemblyInfo.cs
+++ NuUpdate/Properties/AssemblyInfo.cs
@@ -21,15 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("40fc8d57-1059-49c2-b753-69c8dc0ea772")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
