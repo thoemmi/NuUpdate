a053c3e1875e8889160a147742a925f63b1b3b58
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-09-14 22:04:33 +0200

    embed referenced assemblies in installer so we get a single file

--- NuUpdate.Installer/App.xaml.cs
+++ NuUpdate.Installer/App.xaml.cs
@@ -1,6 +1,32 @@
-﻿using System.Windows;
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Windows;
 
 namespace NuUpdate.Installer {
-    public partial class App : Application {
+public partial class App : Application {
+    public App() {
+        AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
     }
+
+    private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args) {
+        var executingAssembly = Assembly.GetExecutingAssembly();
+        var assemblyName = new AssemblyName(args.Name);
+
+        var path = assemblyName.Name + ".dll";
+        if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false) {
+            path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+        }
+
+        using (var stream = executingAssembly.GetManifestResourceStream(path)) {
+            if (stream == null)
+                return null;
+
+            var assemblyRawBytes = new byte[stream.Length];
+            stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+            return Assembly.Load(assemblyRawBytes);
+        }
+    }
+}
 }
