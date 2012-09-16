0302cbf385eacd876d1b2b2f58684c114f1903a5
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-09-16 16:09:49 +0200

    moved shortcut handling to separate class

---
+++ NuUpdate/ShortcutHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using NLog;
+using NuUpdate.Interop;
+
+namespace NuUpdate {
+    internal class ShortcutHandler {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private readonly PathProvider _pathProvider;
+
+        public ShortcutHandler(PathProvider pathProvider) {
+            _pathProvider = pathProvider;
+        }
+
+        public void CreateShortcuts(UpdateInfo updateInfo) {
+            var appPath = _pathProvider.GetAppPath(updateInfo);
+
+            var instructions = updateInfo.GetUpdateInstructions();
+            if (instructions != null && instructions.Shortcuts != null && instructions.Shortcuts.Length > 0) {
+                foreach (var shortcut in instructions.Shortcuts) {
+                    try {
+                        var lnkFilename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), shortcut.Title + ".lnk");
+                        var target = Path.Combine(appPath, shortcut.TargetPath);
+                        if (!File.Exists(target)) {
+                            _logger.Warn("File \"{0}\" does not exist, nevertheless we'll create the shortcut.");
+                        }
+                        new ShellLink {
+                            Target = target,
+                            Description = shortcut.Description,
+                            Arguments = shortcut.Arguments,
+                            IconPath = shortcut.IconPath,
+                            IconIndex = shortcut.IconIndex,
+                        }.Save(lnkFilename);
+                        _logger.Info("Created shortcut for \"{0}\" at \"{1}\".", target, lnkFilename);
+                    }
+                    catch (Exception ex) {
+                        _logger.ErrorException(String.Format("Creating shortcut for \"{0}\" failed", shortcut.TargetPath), ex);
+                    }
+                }
+            } else {
+                _logger.Info("No update instructions found, creating a shortcut for each executable in start menu.");
+                foreach (var exePath in Directory.EnumerateFiles(appPath, "*.exe")) {
+                    try {
+                        var lnkFilename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), Path.GetFileNameWithoutExtension(exePath) + ".lnk");
+                        new ShellLink {
+                            Target = exePath
+                        }.Save(lnkFilename);
+                        _logger.Info("Created shortcut for \"{0}\" at \"{1}\".", exePath, lnkFilename);
+                    }
+                    catch (Exception ex) {
+                        _logger.ErrorException(String.Format("Creating shortcut for \"{0}\" failed", exePath), ex);
+                    }
+                }
+            }            
+        }
+    }
+}
--- NuUpdate/UpdateManager.cs
+++ NuUpdate/UpdateManager.cs
@@ -10,10 +10,10 @@ using NuUpdate.Interop;
 
 namespace NuUpdate {
     public class UpdateManager : IUpdateManager {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
         private readonly string _packageId;
         private readonly Version _currentVersion;
-
-        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         private readonly IPackageRepository _packageRepository;
         private readonly PathProvider _pathProvider;
 
@@ -147,45 +147,7 @@ namespace NuUpdate {
         public Task<UpdateInfo> CreateShortcuts(UpdateInfo updateInfo) {
             return Task.Factory.StartNew(() => {
 
-                var appPath = _pathProvider.GetAppPath(updateInfo);
-
-                var instructions = updateInfo.GetUpdateInstructions();
-                if (instructions != null && instructions.Shortcuts != null && instructions.Shortcuts.Length > 0) {
-                    foreach (var shortcut in instructions.Shortcuts) {
-                        try {
-                            var lnkFilename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), shortcut.Title + ".lnk");
-                            var target = Path.Combine(appPath, shortcut.TargetPath);
-                            if (!File.Exists(target)) {
-                                _logger.Warn("File \"{0}\" does not exist, nevertheless we'll create the shortcut.");
-                            }
-                            new ShellLink {
-                                Target = target,
-                                Description = shortcut.Description,
-                                Arguments = shortcut.Arguments,
-                                IconPath = shortcut.IconPath,
-                                IconIndex = shortcut.IconIndex,
-                            }.Save(lnkFilename);
-                            _logger.Info("Created shortcut for \"{0}\" at \"{1}\".", target, lnkFilename);
-                        }
-                        catch (Exception ex) {
-                            _logger.ErrorException(String.Format("Creating shortcut for \"{0}\" failed", shortcut.TargetPath), ex);
-                        }
-                    }
-                } else {
-                    _logger.Info("No update instructions found, creating a shortcut for each executable in start menu.");
-                    foreach (var exePath in Directory.EnumerateFiles(appPath, "*.exe")) {
-                        try {
-                            var lnkFilename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), Path.GetFileNameWithoutExtension(exePath) + ".lnk");
-                            new ShellLink {
-                                Target = exePath
-                            }.Save(lnkFilename);
-                            _logger.Info("Created shortcut for \"{0}\" at \"{1}\".", exePath, lnkFilename);
-                        }
-                        catch (Exception ex) {
-                            _logger.ErrorException(String.Format("Creating shortcut for \"{0}\" failed", exePath), ex);
-                        }
-                    }
-                }
+                new ShortcutHandler(_pathProvider).CreateShortcuts(updateInfo);
 
                 return updateInfo;
             });
